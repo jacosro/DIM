d7c8b1c86296ddce4d6ec4ed3469224e38d19d9a
Javier Costa <jacosros@gmail.com>
2018-12-17 01:40:39 +0100

    First commit

---
+++ Rosana/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Speech.Recognition;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rosana
+{
+    class RosanaGrammar
+    {
+        private Grammar grammar;
+
+        public RosanaGrammar()
+        {
+            Choices programChoice = new Choices();
+
+
+            SemanticResultValue choiceResultValue = new SemanticResultValue("Explorador de archivos", "explorer.exe");
+            GrammarBuilder resultValueBuilder = new GrammarBuilder(choiceResultValue);
+
+            programChoice.Add(resultValueBuilder);
+        }
+    }
+}
---
+++ Rosana/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+
+namespace Rosana
+{
+    class Program
+    {
+        static SpeechSynthesizer synth = new SpeechSynthesizer();
+        static SpeechRecognitionEngine recognizer;
+        static bool done = false;
+        static bool speechOn = true;
+
+        static void Main(string[] args)
+        {
+            synth.Speak("Hola");
+            synth.Speak("Soy tu asistente virtual");
+            synth.Speak("Me llamo Rosana");
+            synth.Speak("Puedes captar mi atención diciendo mi nombre");
+            synth.Speak("Después dime algo que pueda entender y lo haré");
+
+            Init();
+        }
+
+        private static void Init()
+        {
+            recognizer.SetInputToDefaultAudioDevice();
+            recognizer.UnloadAllGrammars();
+            recognizer.UpdateRecognizerSetting("CFGConfidenceRejectionThresold", 60);
+            // recognizer.LoadGrammar(grammar);
+            recognizer.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(OnSpeechRecognized);
+
+
+            return;
+        }
+
+        private static void OnSpeechRecognized(object sender, SpeechRecognizedEventArgs speechRecognizedEvent)
+        {
+            SemanticValue semantics = speechRecognizedEvent.Result.Semantics;
+
+            string rawText = speechRecognizedEvent.Result.Text;
+            RecognitionResult result = speechRecognizedEvent.Result;
+
+            if (!semantics.ContainsKey("rgb"))
+            {
+                
+            }
+
+        }
+
+
+    }
+}
